bb57fb6c429718f5e4c9c7450faa5a9472c6e15f
Manohar Kumar <manohar.csengineer@gmail.com>
2023-12-08 21:10:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@material-tailwind/react";
+import React from "react";
+import "./App.css";
+
+function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button>Click</Button>
+    </div>
+  );
+}
+
+export default Home;
